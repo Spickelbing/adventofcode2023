61fd4c87d141b1aa1879a5e6742fc29beb8da19e
Jan Heitmüller <jan.heitmueller@tuhh.de>
2023-12-11 20:28:55 +0100

    don't solve day 8 task 2, brute force takes too long

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,6 +1,9 @@
 use eyre::{anyhow, Report};
 use lazy_regex::regex;
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 enum Direction {
     Left,
@@ -30,41 +33,87 @@ fn main() -> Result<(), Report> {
         .collect();
 
     let mut node_to_index_map: HashMap<String, usize> = HashMap::new();
+    let mut start_node_indices = HashSet::<usize>::new();
+    let mut destination_node_indices = HashSet::<usize>::new();
     let mut next_index_to_assign = 0;
+
     for line in input.lines().skip(2) {
         let (node, _) = line.split_once(" = ").unwrap();
         node_to_index_map.insert(node.to_string(), next_index_to_assign);
+
+        if node.ends_with('A') {
+            start_node_indices.insert(next_index_to_assign);
+        } else if node.ends_with('Z') {
+            destination_node_indices.insert(next_index_to_assign);
+        }
+
         next_index_to_assign += 1;
     }
 
-    let mut nodes: Vec<(usize, usize)> = Vec::new();
-    for line in input.lines().skip(2) {
-        let node_regex = regex!(r"^\w{3} = \((\w{3}), (\w{3})\)$");
-        let captures = node_regex.captures(line).unwrap();
+    let nodes: Vec<(usize, usize)> = input
+        .lines()
+        .skip(2)
+        .map(|line| {
+            let node_regex = regex!(r"^\w{3} = \((\w{3}), (\w{3})\)$");
+            let captures = node_regex.captures(line).unwrap();
 
-        let left_node_index = node_to_index_map[&captures[1]];
-        let right_node_index = node_to_index_map[&captures[2]];
+            let left_node_index = node_to_index_map[&captures[1]];
+            let right_node_index = node_to_index_map[&captures[2]];
 
-        nodes.push((left_node_index, right_node_index));
-    }
+            (left_node_index, right_node_index)
+        })
+        .collect();
 
-    let last_node_index = node_to_index_map["ZZZ"];
-    let mut num_steps: usize = 0;
-    let mut current_node_index: usize = node_to_index_map["AAA"];
-    for direction in directions.iter().cycle() {
-        if current_node_index == last_node_index {
-            break;
+    // task 1
+    {
+        let destination_node_index = node_to_index_map["ZZZ"];
+        let mut current_node_index: usize = node_to_index_map["AAA"];
+
+        let mut num_steps: usize = 0;
+        for direction in directions.iter().cycle() {
+            if current_node_index == destination_node_index {
+                break;
+            }
+            num_steps += 1;
+
+            let (left_node_index, right_node_index) = nodes[current_node_index];
+            current_node_index = match direction {
+                Direction::Left => left_node_index,
+                Direction::Right => right_node_index,
+            };
         }
-        num_steps += 1;
 
-        let (left_node_index, right_node_index) = nodes[current_node_index];
-        current_node_index = match direction {
-            Direction::Left => left_node_index,
-            Direction::Right => right_node_index,
-        };
+        println!("Task 1: {num_steps}");
+    };
+
+    // task 2
+    {
+        let mut current_node_indices: HashSet<usize> = start_node_indices.clone();
+        let mut next_node_indices = HashSet::<usize>::with_capacity(current_node_indices.len());
+        let mut num_steps: usize = 0;
+
+        for direction in directions.iter().cycle() {
+            if current_node_indices.is_subset(&destination_node_indices) {
+                break;
+            }
+            num_steps += 1;
+
+            for &node_index in &current_node_indices {
+                let (left_node_index, right_node_index) = nodes[node_index];
+                match direction {
+                    Direction::Left => next_node_indices.insert(left_node_index),
+                    Direction::Right => next_node_indices.insert(right_node_index),
+                };
+            }
+
+            let mut tmp = current_node_indices;
+            tmp.clear();
+            current_node_indices = next_node_indices;
+            next_node_indices = tmp;
+        }
+
+        println!("Task 2: {num_steps}");
     }
-
-    println!("Task 1: {num_steps}");
 
     Ok(())
 }
